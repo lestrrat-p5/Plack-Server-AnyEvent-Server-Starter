655d1d435967af72c8b9a8d48c30cde6949e2c8c
lestrrat <lestrrat+github@gmail.com>
2010-02-16 13:04:06 +0900

    deprecation

--- lib/Plack/Server/AnyEvent/Server/Starter.pm
+++ lib/Plack/Server/AnyEvent/Server/Starter.pm
@@ -68,12 +68,17 @@ Plack::Server::AnyEvent::Server::Starter - Use AnyEvent-Based Plack Apps From Se
 
    % start_server --port=80 -- plackup -s AnyEvent::Server::Starter
 
+=head1 DEPRECATION WARNING
+
+This module is now merged with Twiggy, and is now deprecated. Please use Plack 0.99+ and Twiggy instead.
+
 =head1 DESCRIPTION
 
 Plack::Server::AnyEvent::Server::Starter is a wrapper to manage L<Plack::Server::AnyEvent> using L<Server::Starter>. Use this module when, for example, you want to run a L<Tatsumaki> app (which is AnyEvent based) via L<Server::Starter>.
 
 =head1 SEE ALSO
 
+L<Twiggy>
 L<Server::Starter>
 L<Plack::Server::AnyEvent>
 L<Plack::Server::Standalone::Prefork::Server::Starter>
